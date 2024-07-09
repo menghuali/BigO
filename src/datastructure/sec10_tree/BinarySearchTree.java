20219527ce91c17af4fc99e98e0418ba2a805f92
Menghua Li <menghua.li@westjet.com>
2024-07-08 20:58:21 -0400

    Improved BST remvoe

--- src/datastructure/sec10_tree/BinarySearchTree.java
+++ src/datastructure/sec10_tree/BinarySearchTree.java
@@ -55,64 +55,80 @@ public class BinarySearchTree {
      * right)
      * 2.2 Replace the node to remove with the min node
      * 3. If the node to remove has no right child, replace it with the left child
+     * 
+     * Sample tree:
      */
     public void remove(int value) {
-        Node[] toRemoveAndParent = lookupNodeAndParent(value);
-        Node toRemove = toRemoveAndParent[0];
-        Node parentOfToRemvoe = toRemoveAndParent[1];
-        if (toRemove == null)
+        // 1. If root is empty, exit method.
+        if (root == null)
             return;
-        if (toRemove.getRight() != null) {
-            Node[] minAndParent = findMinAndParent(toRemove.getRight());
-            Node min = minAndParent[0];
-            Node parentOfMin = minAndParent[1];
-            if (parentOfMin == null) {
-                toRemove.setRight(min.getRight());
+        // 2. Find the node to remove and the parent node.
+        Node currentNode = root;
+        Node parent = null;
+        while (currentNode != null) {
+            if (value < currentNode.getValue()) {
+                parent = currentNode;
+                currentNode = currentNode.getLeft();
+            } else if (value > currentNode.getValue()) {
+                parent = currentNode;
+                currentNode = currentNode.getRight();
             } else {
-                parentOfMin.setLeft(min.getRight());
-            }
-            toRemove.setValue(min.getValue());
-        } else {
-            if (parentOfToRemvoe == null) {
-                root = root.getLeft();
-            } else {
-                if (parentOfToRemvoe.getLeft() == toRemove) {
-                    parentOfToRemvoe.setLeft(toRemove.getLeft());
-                } else {
-                    parentOfToRemvoe.setRight(toRemove.getLeft());
-                }
+                remvoeNode(currentNode, parent);
+                break;
             }
         }
+
         print();
     }
 
-    private Node[] lookupNodeAndParent(int value) {
-        Node found = null;
-        Node parent = null;
-        Node current = root;
-        while (current != null) {
-            if (value == current.getValue()) {
-                found = current;
-                break;
-            } else if (value < current.getValue()) {
-                parent = current;
-                current = current.getLeft();
+    private void remvoeNode(Node toRemove, Node parent) {
+        if (toRemove.getLeft() == null && toRemove.getRight() == null) {
+            // 1. Leaf node
+            replaceWith(toRemove, null, parent);
+        } else if (toRemove.getRight() == null) {
+            // 2. If node to remove only has left child
+            replaceWith(toRemove, toRemove.getLeft(), parent);
+        } else if (toRemove.getRight() != null) {
+            // 3. If node to remove has right child
+            Node child = toRemove.getRight();
+            if (child.getLeft() == null) {
+                // 3.1 If right child node doesn't have left child node
+                replaceWith(toRemove, child, parent);
             } else {
-                parent = current;
-                current = current.getRight();
+                // 3.2 If right child node has left child node
+                replaceWithMinRightChild(toRemove, parent);
             }
         }
-        return new Node[] { found, parent };
     }
 
-    private Node[] findMinAndParent(Node node) {
-        Node min = node;
-        Node parent = null;
+    private void replaceWithMinRightChild(Node toBeReplaced, Node parent) {
+        Node parentOfMin = toBeReplaced.getRight();
+        Node min = parentOfMin.getLeft();
+        // Find the min node from the right brach of the node to be replaced
         while (min.getLeft() != null) {
-            parent = min;
+            parentOfMin = min;
             min = min.getLeft();
         }
-        return new Node[] { min, parent };
+        // Replace the left child of min-parent with the right child of min
+        replaceWith(min, min.getRight(), parentOfMin);
+
+        // Set child nodes of min to those of the to-be-placed node
+        min.setLeft(toBeReplaced.getLeft());
+        min.setRight(toBeReplaced.getRight());
+        // Replace with min node
+        replaceWith(toBeReplaced, min, parent);
+    }
+
+    private void replaceWith(Node toBeReplaced, Node replacement, Node parent) {
+        if (parent == null) {
+            root = replacement;
+        } else {
+            if (toBeReplaced == parent.getLeft()) {
+                parent.setLeft(replacement);
+            } else {
+                parent.setRight(replacement);
+            }
+        }
     }
 
     public void print() {
@@ -127,9 +143,14 @@ public class BinarySearchTree {
         bst.insert(1);
         bst.insert(6);
         bst.insert(15);
+        bst.insert(13);
+        bst.insert(18);
         bst.insert(170);
-        // bst.print();
-
+        bst.insert(100);
+        bst.insert(120);
+        bst.insert(110);
+        bst.insert(150);
+        bst.print();
         bst.remove(20);
     }
 }
