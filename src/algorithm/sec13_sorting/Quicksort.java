2357ae20753ef3babca3e104df3d325eb1bb754a
Menghua Li <menghua.li@westjet.com>
2024-07-16 22:55:17 -0400

    Improved Quicksort

--- src/algorithm/sec13_sorting/Quicksort.java
+++ src/algorithm/sec13_sorting/Quicksort.java
@@ -2,74 +2,6 @@ package algorithm.sec13_sorting;
 
 public class Quicksort {
 
-    public static int[] sort(int[] numbers) {
-        if (numbers.length == 1) {
-            return numbers;
-        } else if (numbers.length == 2) {
-            if (numbers[0] > numbers[1]) {
-                var temp = numbers[0];
-                numbers[0] = numbers[1];
-                numbers[1] = temp;
-            }
-            return numbers;
-        } else {
-            int pivot = pivot(numbers);
-            return merge(sort(subarray(numbers, 0, pivot)), numbers[pivot],
-                    sort(subarray(numbers, pivot + 1, numbers.length)));
-        }
-    }
-
-    private static int[] merge(int[] left, int pivot, int[] right) {
-        int leftLen = (left == null || left.length == 0) ? 0 : left.length;
-        int rightLen = (right == null || right.length == 0) ? 0 : right.length;
-        var merge = new int[leftLen + rightLen + 1];
-        int i = 0;
-        if (left != null && left.length > 0)
-            for (; i < left.length; i++) {
-                merge[i] = left[i];
-            }
-        merge[i++] = pivot;
-        if (right != null && right.length > 0)
-            for (int j = 0; j < right.length; j++) {
-                merge[i++] = right[j];
-            }
-        return merge;
-    }
-
-    private static int[] subarray(int[] array, int start, int end) {
-        var len = end - start;
-        int[] subarray = null;
-        if (len > 0) {
-            subarray = new int[len];
-            for (int i = 0; i < subarray.length; i++) {
-                subarray[i] = array[start + i];
-            }
-        }
-        return subarray;
-    }
-
-    private static int pivot(int[] numbers) {
-        int left = 0, right = numbers.length - 1;
-        while (left < right) {
-            if (numbers[left] <= numbers[right]) {
-                left++;
-            } else {
-                var temp = numbers[left];
-                numbers[left] = numbers[right - 1];
-                numbers[right - 1] = numbers[right];
-                numbers[right] = temp;
-                right--;
-            }
-        }
-        return right;
-    }
-
-    public static void main(String[] args) {
-        int[] numbers = new int[] { 3, 7, 8, 5, 2, 1, 9, 5, 4 };
-        print(sort(numbers));
-
-    }
-
     private static void print(int[] numbers) {
         if (numbers == null || numbers.length == 0) {
             System.out.println("[]");
@@ -80,6 +12,51 @@ public class Quicksort {
             sb.append(numbers[i]).append(", ");
         }
         System.out.println(sb.toString());
+    }
+
+    public static void sort(int[] numbers) {
+        if (numbers.length > 1)
+            pivot(numbers, 0, numbers.length - 1);
+        print(numbers);
+    }
+
+    private static void pivot(int[] numbers, int left, int right) {
+        int len = right - left + 1;
+        if (len == 2) {
+            // Swap the number if left and right is adjacent and left is greater than left.
+            if (numbers[left] > numbers[right]) {
+                var temp = numbers[left];
+                numbers[left] = numbers[right];
+                numbers[right] = temp;
+            }
+        } else if (len > 2) {
+            int l = left;
+            int r = right;
+            // Pick the last element as 'pivot' and devide the current range of the array
+            while (l < r)
+                if (numbers[l] <= numbers[r]) {
+                    l++;
+                } else {
+                    var temp = numbers[l];
+                    numbers[l] = numbers[r - 1];
+                    numbers[r - 1] = numbers[r];
+                    numbers[r] = temp;
+                    r--;
+                }
+            // Pivot the sub-range left to the pivot
+            pivot(numbers, left, r - 1);
+            // Pivot the sub-range right to the pivot
+            pivot(numbers, r + 1, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[] { 32, 88, 194, 165, 95, 126, 67, 187, 94, 167, 168, 111, 152, 139, 138, 80, 196, 136,
+                20, 149, 91, 75, 40, 108, 21, 44, 47, 84, 189, 74, 73, 34, 181, 43, 99, 114, 7, 191, 120, 24, 129, 121,
+                42, 79, 53, 83, 140, 182, 132, 190, 137, 127, 109, 169, 19, 4, 71, 86, 173, 171, 63, 105, 119, 100, 125,
+                160, 145, 48, 90, 153, 131, 41, 155, 28, 143, 17, 157, 46, 103, 144, 45, 72, 26, 177, 11, 116, 193, 25,
+                164, 98, 8, 148, 60, 154, 198, 128, 115, 69, 22, 61 };
+        sort(numbers);
     }
 
 }
